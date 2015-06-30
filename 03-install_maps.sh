3d174258149394b8350cb65504b19d5fd73dfa31
Pando85 <pando855@gmail.com>
2015-07-01 00:45:56 +0200

    Fixed check if there are any map to install

--- 03-install_maps.sh
+++ 03-install_maps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## install additional maps
-if [ -d "/maps" ]; then
+if [ -f /maps/*.pk3 ]; then
     echo -e "\n========================== installing additional maps =========================="
     cp -vf /maps/*.pk3 /UrbanTerror42/q3ut4
 fi
